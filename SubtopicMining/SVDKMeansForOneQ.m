571647a5026577ff11686ae4d3939dfe0f2d2a61
chenyanxiong <xiongchenyan@gmail.com>
2014-10-26 16:59:09 -0400

    debug

--- SubtopicMining/SVDKMeansForOneQ.m
+++ SubtopicMining/SVDKMeansForOneQ.m
@@ -14,7 +14,7 @@ M=U(:,1:ReducedDim);
 fprintf('reduced to [%d][%d]\n',size(M,1),size(M,2));
 [idx,C,sumd,D]=kmeans(M,k,'emptyaction','singleton');
 
-D = D ./(D* ones(size(D,1),1) * ones(1,size(D,2)));
+D = D ./(D* ones(size(D,2),1) * ones(1,size(D,2)));
 csvwrite(D,OutName);
 flag=  1;
 end
--- SubtopicMining/Word2vecKMeans.m
+++ SubtopicMining/Word2vecKMeans.m
@@ -4,7 +4,7 @@ function flag = Word2vecKMeans(InName,OutName,k)
 X = csvread(InName);
 fprintf('read data [%d][%d]\n',size(X,1),size(X,2));
 [idx,C,sumd,D]=kmeans(X,k,'emptyaction','singleton');
-D = D ./(D* ones(size(D,1),1) * ones(1,size(D,2)));
+D = D ./(D* ones(size(D,2),1) * ones(1,size(D,2)));
 csvwrite(D,OutName);
 flag=  1;
 
