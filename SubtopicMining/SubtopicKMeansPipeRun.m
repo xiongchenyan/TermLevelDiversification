8dfd6e94b9ff56993e19f5812873ed45641f107a
chenyanxiong <xiongchenyan@gmail.com>
2014-10-26 16:09:03 -0400

    matlab model implemented

--- SubtopicMining/SubtopicKMeansPipeRun.m
+++ SubtopicMining/SubtopicKMeansPipeRun.m
@@ -4,11 +4,23 @@ function flag = SubtopicKMeansPipeRun(DataDir)
     % cluster word-word cooccurance matrix
     % cluster SVDed w-w cooc matrix
     % cluster word2vec matrix
-    
+
+k = 10;
+DimReduce = 300;
 for qid=1:200
+    fprintf('start clustering [%d]\n',qid);
     RawCoocInName = strcat(DataDir,'/',int2str(qid),'_occur');
     RawKmeansOutName = strcat(RawCoocInName,'_kmeans');
     SVDKmeansOutName = strcat(RawCoocInName,'_svdkmeans');
-
+    WordvecInName = strcat(DataDir,'/',int2str(qid),'_word2vec');
+    WordvecOutName =  strcat(WordvecInName,'_kmeans');    
+    KMeansForOneQ(RawCoocInName,RawKmeansOutName,k);
+    fprintf('kmeans done\n');
+    SVDKmeansForOneQ(RawCoocInName,SVDKmeansOutName,k,DimReduce);
+    fprintf('svd kmeans done\n');
+    Word2vecKMeans(WordvecInName,WordvecOutName,k);
+    fprintf('wordvec kmeans done\n');
 end
+flag = 1;
+return
 
