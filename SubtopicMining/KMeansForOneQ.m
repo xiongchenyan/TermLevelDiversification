3ca14bc4b8187fe91dd5cc6683b565639d45ed03
chenyanxiong <xiongchenyan@gmail.com>
2014-10-26 16:58:01 -0400

    debug

--- SubtopicMining/KMeansForOneQ.m
+++ SubtopicMining/KMeansForOneQ.m
@@ -11,7 +11,7 @@ X=M;
 
 fprintf('read data [%d][%d]\n',size(X,1),size(X,2));
 [idx,C,sumd,D]=kmeans(X,k,'emptyaction','singleton');
-D = D ./(D* ones(size(D,1),1) * ones(1,size(D,2)));
+D = D ./(D* ones(size(D,2),1) * ones(1,size(D,2)));
 csvwrite(D,OutName);
 flag=  1;
 end
